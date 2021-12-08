d3081c4b28dc2569924ed548acc8e5576be8a78d
unknown <phkhanh1188@gmail.com>
2021-12-08 09:28:47 +0700

    them trang dua tien

---
+++ tazaform.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TAZA-GROUP TEST</title>
+</head>
+<body>
+	<H1>LỰA CHỌN 1: QUẢN LÝ DANH MỤC SẢN PHẨM</H1>
+	<h3>BÀI TEST CỦA ỨNG VIÊN HOÀNG KHÁNH</h3>
+	
+</body>
+</html>
